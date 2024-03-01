e00b17cb7fa55f5a2ab8e265a0f53ef3f620041c
Cara Manning <cara.manning@uconn.edu>
2024-02-29 20:08:48 -0500

    updates to transect plot

---
+++ LIS/plot_LISGC_transect.m
@@ -0,0 +1,182 @@
+load LISAug23_CH4N2O_CTD.mat
+LIS = LISAug23_CH4N2O_CTD;
+
+load LISAug2023CastData.mat;
+
+ncast = 16:22;
+Cast = LISAug2023CastData.Cast(ncast);
+Lat = LISAug2023CastData.Lat(ncast);
+Lon = LISAug2023CastData.Lon(ncast);
+
+km_between = m_lldist(Lon,Lat); % distance between consecutive stations
+km_between = [0; km_between]; % add on zero for first station
+
+km_cumulative = cumsum(km_between); % consecutive distance
+
+%%
+
+
+
+figure(1)
+clf; hold on;
+
+subplot(1,3,1)
+hold on; box on;
+stn = 'EXCR-cast01';
+A = find(LIS.Station==stn);
+errorbar(LIS.mean_CH4_nM(A),LIS.Depth(A),LIS.std_CH4_nM(A),'horizontal','o-b','linewidth',1.5, 'markerfacecolor','b');
+
+stn = 'EXCR-cast02';
+A = find(LIS.Station==stn);
+errorbar(LIS.mean_CH4_nM(A),LIS.Depth(A),LIS.std_CH4_nM(A),'horizontal','s-r','linewidth',1.5, 'markerfacecolor','r');
+legend('EXCR-cast01','EXCR-cast02','location','northeast')
+
+xlabel('CH_4 (nM)');
+ylabel('Depth (m)');
+axis ij;
+
+subplot(1,3,2)
+hold on; box on;
+stn = 'EXCR-cast01';
+A = find(LIS.Station==stn);
+plot(LIS.T(A),LIS.Depth(A),'o-b','linewidth',1.5, 'markerfacecolor','b');
+
+stn = 'EXCR-cast02';
+A = find(LIS.Station==stn);
+plot(LIS.T(A),LIS.Depth(A),'s-r','linewidth',1.5, 'markerfacecolor','r');
+legend('EXCR-cast01','EXCR-cast02','location','northeast')
+
+xlabel('Temp (^oC)');
+ylabel('Depth (m)');
+axis ij;
+
+subplot(1,3,3)
+hold on; box on;
+stn = 'EXCR-cast01';
+A = find(LIS.Station==stn);
+plot(LIS.O2_umolkg(A),LIS.Depth(A),'o-b','linewidth',1.5, 'markerfacecolor','b');
+
+stn = 'EXCR-cast02';
+A = find(LIS.Station==stn);
+plot(LIS.O2_umolkg(A),LIS.Depth(A),'s-r','linewidth',1.5, 'markerfacecolor','r');
+legend('EXCR-cast01','EXCR-cast02','location','northeast')
+
+xlabel('O_2 (\mumol/kg)');
+ylabel('Depth (m)');
+axis ij;
+
+
+%%
+% we need to make a grid that is evenly spaced so that all the casts are
+% interpolated onto the same spacing
+
+dl = [0:0.25:18]'; % depth spacing for interpolation
+x = km_cumulative'; % temporary x values for interpolation
+n_stn = numel(x);
+
+dl_grid = repmat(dl,1,n_stn);
+
+x_grid = repmat(x,length(dl),1);
+
+CH4i = nan(numel(dl),n_stn); % make a blank grid for storing CH4
+N2Oi = nan(numel(dl),n_stn); % make a blank grid for storing CH4
+O2i = nan(numel(dl),n_stn); % make a blank grid for storing CH4
+
+
+stn = 'EXR1-cast01';
+A = find(LIS.Station==stn);
+CH4i(:,1) = interp1(LIS.Depth(A),LIS.mean_CH4_nM(A),dl);
+N2Oi(:,1) = interp1(LIS.Depth(A),LIS.mean_N2O_nM(A),dl);
+O2i(:,1) = interp1(LIS.Depth(A),LIS.O2_umolkg(A),dl);
+
+
+stn = 'EXCR-cast02';
+A = find(LIS.Station==stn);
+CH4i(:,2) = interp1(LIS.Depth(A),LIS.mean_CH4_nM(A),dl);
+N2Oi(:,2) = interp1(LIS.Depth(A),LIS.mean_N2O_nM(A),dl);
+O2i(:,2) = interp1(LIS.Depth(A),LIS.O2_umolkg(A),dl);
+
+
+stn = 'MID3-cast01';
+A = find(LIS.Station==stn);
+CH4i(:,3) = interp1(LIS.Depth(A),LIS.mean_CH4_nM(A),dl);
+N2Oi(:,3) = interp1(LIS.Depth(A),LIS.mean_N2O_nM(A),dl);
+O2i(:,3) = interp1(LIS.Depth(A),LIS.O2_umolkg(A),dl);
+
+stn = 'MID4-cast14';
+A = find(LIS.Station==stn);
+CH4i(:,4) = interp1(LIS.Depth(A),LIS.mean_CH4_nM(A),dl);
+N2Oi(:,4) = interp1(LIS.Depth(A),LIS.mean_N2O_nM(A),dl);
+O2i(:,4) = interp1(LIS.Depth(A),LIS.O2_umolkg(A),dl);
+
+
+stn = 'MID5-cast01';
+A = find(LIS.Station==stn);
+CH4i(:,5) = interp1(LIS.Depth(A),LIS.mean_CH4_nM(A),dl);
+N2Oi(:,5) = interp1(LIS.Depth(A),LIS.mean_N2O_nM(A),dl);
+O2i(:,5) = interp1(LIS.Depth(A),LIS.O2_umolkg(A),dl);
+
+
+stn = 'WLIS-cast02';
+A = find(LIS.Station==stn);
+CH4i(:,6) = interp1(LIS.Depth(A),LIS.mean_CH4_nM(A),dl);
+N2Oi(:,6) = interp1(LIS.Depth(A),LIS.mean_N2O_nM(A),dl);
+O2i(:,6) = interp1(LIS.Depth(A),LIS.O2_umolkg(A),dl);
+
+
+stn = 'WLI6-cast01';
+A = find(LIS.Station==stn);
+[~,Ai] = sort(LIS.Depth(A));
+
+CH4i(:,7) = interp1(LIS.Depth(A(Ai)),LIS.mean_CH4_nM(A(Ai)),dl);
+N2Oi(:,7) = interp1(LIS.Depth(A(Ai)),LIS.mean_N2O_nM(A(Ai)),dl);
+O2i(:,7) = interp1(LIS.Depth(A),LIS.O2_umolkg(A),dl);
+
+
+
+
+figure(2)
+clf; hold on;
+box on;
+set(gca,'tickdir','out');
+clevel = [40:1:450];
+C = contourf(x_grid,dl_grid,CH4i,clevel,'edgecolor','none');
+xlabel('Transect distance [km]');
+ylabel('Depth [m]');
+c = colorbar;
+c.Label.String = 'CH_4 (nM)';
+c.Label.FontSize = 16;
+set(gca,'layer','top');
+axis ij;
+
+figure(3)
+clf; hold on;
+box on;
+set(gca,'tickdir','out');
+clevel = [8:0.05:13.5];
+C = contourf(x_grid,dl_grid,N2Oi,clevel,'edgecolor','none');
+xlabel('Transect distance [km]');
+ylabel('Depth [m]');
+c = colorbar;
+c.Label.String = 'N_2O (nM)';
+c.Label.FontSize = 16;
+set(gca,'layer','top');
+axis ij;
+
+
+figure(4)
+clf; hold on;
+box on;
+set(gca,'tickdir','out');
+set(gca,'fontsize',14);
+clevel = [40:1:200];
+C = contourf(x_grid,dl_grid,O2i,clevel,'edgecolor','none');
+xlabel('Transect distance [km]');
+ylabel('Depth [m]');
+c = colorbar;
+c.Label.String = 'O_2 (\mumol/kg)';
+c.Label.FontSize = 16;
+set(gca,'layer','top');
+axis ij;
+
+
