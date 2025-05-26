60bc79593d571650370b0baecfcb7051b10e13dc
Cara Manning <cara.manning@uconn.edu>
2025-05-26 19:27:53 -0400

    update to flux plots

---
+++ LIS/LIS_gas_flux_LISICOS.m
@@ -0,0 +1,312 @@
+% this code calculates the gas flux based on NDBC mooring data collected at
+% one location (mooring 44022, EXRX)
+% data from mooring 44040, WLIS, was not available for the desired time
+% period
+
+load LISAug23_CH4N2O_CTD.mat;
+
+surf_i = find(LISAug23_CH4N2O_CTD.Depth<3);
+
+LIS_s = LISAug23_CH4N2O_CTD(surf_i,:);
+
+%%
+% atmospheric concentrations averaged over the 3 cruises
+N2Oatm_dry = 338e-9; %dry mole fraction
+CH4atm_dry = 2020e-9; %dry mole fraction
+
+%%
+
+% load in wind data
+%load NDBC_44040h2023.mat;
+% winds are collected at 3.5 m height and need to be adjusted to u10
+load exrxMet_adj.mat;
+% 
+% mbar_to_atm = 1./1013.25; % convert pressure data from mbar to atm for use in gas sol eqns
+% 
+% exrxMet_adj.baroPress_atm = exrxMet_adj.baroPress_Avg .* mbar_to_atm;
+
+
+wd = exrxMet_adj;
+%%
+dl = datetime(2023,7,1,0,0,0):15/60/24:datetime(2023,8,31,0,0,0);
+
+% find the unique values in exrx
+[unique_times_exrx, ~, idx] = unique(wd.TIMESTAMP);  % idx gives the group index for each key
+num_keys = numel(unique_times_exrx);
+avg_wspd_exrx = accumarray(idx(:), wd.windSpd_MS(:), [], @mean);
+avg_slp_exrx = accumarray(idx(:), wd.baroPress_atm(:), [], @mean);
+
+i_wspd_exrx = interp1(unique_times_exrx,avg_wspd_exrx,dl); % interpolate to fill in the gaps
+i_slp_exrx = interp1(unique_times_exrx,avg_slp_exrx,dl); % interpolate to fill in the gaps
+
+wspd_exrx_dl = nan(size(dl));
+
+for i = 1:length(dl)
+    A = find(unique_times_exrx == dl(i));
+    if ~isempty(A)
+        wspd_exrx_dl(i) = avg_wspd_exrx(A);
+    end;
+end;
+
+% get a list of the nan values
+for i = 1:length(i_exrx)
+    A = find(unique_times_exrx == dl(i));
+    if ~isempty(A)
+        wspd_exrx_dl(i) = avg_wspd_exrx(A);
+    end;
+end;
+
+%%
+%b = wd;
+
+b.datenum = datenum(dl);
+%%
+b.slp = i_slp_exrx; % slp
+time_int_wind_hr = 0.25; % interval between wind speed observations = 0.25 hr
+
+b.WSPD = i_wspd_exrx;
+h_WSPD = 3.5; % wind speed height in meters
+b.u10 = b.WSPD .*(10/3.5)^0.11; % correcting to 10 m height
+
+%%
+
+% slp_mbar = ERA5.slp_Pa./100;
+% mbar_to_atm = 1./1013.25; % convert pressure data from mbar to atm for use in gas sol eqns
+% slp_atm = slp_mbar .* mbar_to_atm;
+
+%clear slp_mbar;
+
+% rename the imported files
+%raice = demo_AMSR2; 
+%rawind = ERA5.wspd;
+%raslp = ERA5.slp_atm;
+%gd = demo_gas_data;
+
+wt_t_30 = 30; % weighting time of 30 days
+wt_t_60 = 60; % weighting time of 60 days
+
+% we want the wind to have dimesions time x lat x lon
+% but ERA5.wspd has dimensions lon x lat x time so we need to permute
+
+% rawind.wind = permute(ERA5.wspd,[3,2,1]);
+% rawind.lat = ERA5.lat;
+% rawind.lon = ERA5.lon;
+% rawind.datet = ERA5.datetime;
+% rawind.daten = datenum(ERA5.datetime);
+
+% we want the slp to have dimesions time x lat x lon
+% but ERA5.slp has dimensions lon x lat x time so we need to permute
+% raslp.slp = permute(slp_atm,[3,2,1]);
+% raslp.lat = ERA5.lat;
+% raslp.lon = ERA5.lon;
+% raslp.datet = ERA5.datetime;
+% raslp.daten = datenum(ERA5.datetime);
+
+%clear slp_atm;
+
+
+% datetimes
+gd.dn = LIS_s.datetime;
+
+
+% fake mld
+gd.mld = 5.*ones(size(gd.dn));
+
+gd.lat = LIS_s.Lat;
+
+gd.lon = LIS_s.Lon;
+
+gd.P = LIS_s.P;
+
+gd.S = LIS_s.S;
+
+gd.T = LIS_s.T;
+
+gd.depth = LIS_s.Depth;
+
+gd.station = LIS_s.Station;
+
+gd.n2o_nmolkg = LIS_s.N2O_mean_nmolkg;
+gd.ch4_nmolkg = LIS_s.CH4_mean_nmolkg;
+
+
+
+% calculate n2o concentration with water vapor pressure at saturation
+% c_H2Osat = c_dry * (1 - H2Opress)
+N2Oatm_H2Osat = N2Oatm_dry .* (1 - vpress(gd.S,gd.T));
+gd.n2o_eq_nmolkg = N2Osol(gd.S,gd.T,N2Oatm_H2Osat).*1000;
+
+CH4atm_H2Osat = CH4atm_dry .* (1 - vpress(gd.S,gd.T));
+gd.ch4_eq_nmolkg = CH4sol(gd.S,gd.T,CH4atm_H2Osat)'.*1000;
+
+
+% save the gas data to new variable names
+%s_time = datenum(gd.yyyy,gd.mm,gd.dd,gd.HH,gd.MM,gd.SS);
+s_time = datenum(gd.dt);
+%s_lat = gd.lat;
+%s_lon = gd.lon;
+s_mld = gd.mld;
+s_T = gd.T;
+s_S = gd.S;
+s_P = gd.P;
+s_ch4 = gd.ch4_nmolkg;
+s_n2o = gd.n2o_nmolkg;
+s_ch4_eq = gd.ch4_eq_nmolkg;
+s_n2o_eq = gd.n2o_eq_nmolkg;
+s_station = gd.station;
+s_depth = gd.depth;
+
+%%
+
+%und.lat = s_lat; 
+%und.lon= s_lon;
+% %%
+% %--- Wind matrix
+% % Interpolate 3D wind matrix to the observations
+% % need to use numeric date in the ndgrid function
+% % here X_wind is date, Y_wind is lat, Z_wind is lon
+% %und.wind = interp1(und)
+% 
+% [X_wind,Y_wind,Z_wind] = ndgrid(rawind.daten,rawind.lat,rawind.lon);
+% V_wind = rawind.wind;
+% Y_wind = double(Y_wind);
+% Z_wind = double(Z_wind);
+% % calculate instantaneous wind speeds for time of each sample
+% und.wind = interpn(X_wind,Y_wind,Z_wind,V_wind,und.time,und.lat,und.lon); % need to turn the Y and Z into doubles
+% 
+% %--- SLP matrix
+% % Interpolate 3D SLP matrix to the observations
+% [X_slp,Y_slp,Z_slp] = ndgrid(raslp.daten,raslp.lat,raslp.lon);
+% V_slp = raslp.slp;
+% Y_slp = double(Y_slp);
+% Z_slp = double(Z_slp);
+% und.slp = interpn(X_slp,Y_slp,Z_slp,V_slp,und.time,und.lat,und.lon); %instantaneous slp for time of cruise
+%%
+% create structure to save the interpolated data
+und.time= s_time; 
+
+% calculate instantaneous wind speeds for time of each sample
+und.wind = interpn(b.datenum,b.u10,und.time); % need to turn the Y and Z into doubles
+
+%-------------- instantaneous and 30-day weighted fluxes -----------------
+%--- Create historical wind and slp matrices for the samples using the
+% weighting period wt_t_30 (30 days) 
+wt_t = wt_t_30;
+int = time_int_wind_hr; % time interval for wind speed and slp observations
+lag = 1:(wt_t *(24/int)); % # observations back in time (1 : # days * # observations per day)
+windmat = nan(length(und.time),length(lag)+1); % empty matrix; note: the +1 is to hold the current (i.e. 0 hr lag) observation
+t_back = nan(length(und.time),length(lag)+1);
+
+slp_t = wt_t_30;
+lag_slp = 1:(slp_t *(24/int));
+t_back_slp = nan(length(und.time),length(lag)+1);
+slpmat = nan(length(und.time),length(lag_slp)+1); %empty matrix; note: the +1 is to hold the current (i.e. 0 hr lag) observation
+
+
+for kk=fliplr(lag)
+    t_back(:,kk) = und.time - datenum(0,0,0,kk*int,0,0); %lag sample time backwards by kk*int hours (e.g. if kk = 120 and int = 6 hrs ==> lag backwards 720 hrs (30 days)
+    wind_back = interpn(b.datenum,b.WSPD,t_back(:,kk)); %interpolate to lat/lon/time grid - wind speed lagged backwards, interpolated onto cruise track grid
+    %wind_back = interpn(X_wind,Y_wind,Z_wind,V_wind,t_back(:,kk),und.lat,und.lon); %interpolate to lat/lon/time grid - wind speed lagged backwards, interpolated onto cruise track grid
+    
+    windmat(:,lag(end)-lag(kk)+1) = wind_back; %fill matrix
+end
+t_back(:,end) = und.time;
+windmat(:,end) = und.wind; %add instantaneous wind
+und.windmat = windmat;
+%%
+%for kk=fliplr(lag_slp)
+%    t_back_slp(:,kk) = und.time - datenum(0,0,0,kk*int,0,0); %lag sample time backwards by kk*int hours (e.g. if kk = 120 and int = 6 hrs ==> lag backwards 720 hrs (30 days)
+%    slp_back = interpn(X_slp,Y_slp,Z_slp,V_slp,t_back_slp(:,kk),und.lat,und.lon); %interpolate to lat/lon/time grid - wind speed lagged backwards, interpolated onto cruise track grid
+%    slpmat(:,lag(end)-lag(kk)+1) = slp_back; %fill matrix
+%end
+
+%slpmat(:,end) = und.slp; %add instantaneous slp
+%und.slpmat = slpmat;
+
+% temporarily putting in 1 values for pressure
+slpmat = ones(size(und.windmat));
+und.slpmat = ones(size(und.windmat));
+
+% clear variables no longer needed
+clear  X_wind Y_wind Z_wind V_wind ...
+    X_slp Y_slp Z_slp V_slp ...
+    kk t_back wind_back uw_wind
+
+% SKIPPING THE ICE FRACTION CORRECTION SINCE THIS IS ICE-FREE
+% using kw_weighting rather than kw_weighting_ice
+
+%--- make matrices of T, S, MLD, and Schmidt number
+% Assume constant backwards in time
+Tmat = repmat(s_T,1,length(lag)+1);
+Smat = repmat(s_S,1,length(lag)+1);
+zMLmat = repmat(s_mld,1,length(lag)+1);
+
+[~,Scmat_CH4] = gasmoldiff(Smat,Tmat,'CH4');
+[~,Scmat_N2O] = gasmoldiff(Smat,Tmat,'N2O');
+
+% calculate open water gas transfer velocity following Wanninkhof et al.
+% (2014)
+spd = 60*60*24; % seconds per day
+k.CH4 = kgas(windmat,Scmat_CH4,'W14').* spd; % m/d
+k.N2O = kgas(windmat,Scmat_N2O,'W14') .* spd; % m/d
+
+%ice = und.icemat;
+
+slp_inst = slpmat(:,end); % instantaneous slp
+
+% calculate instantaneous gas transfer velocity for ice-free condition
+gd.k_inst_CH4 = k.CH4(:,1); % instantaneous CH4 gas tranfer velocity in m/d
+gd.k_inst_N2O = k.N2O(:,1); % instantaneous N2O gas tranfer velocity in m/d
+
+% instantenous sea-air gas flux corrected for slp
+gd.F_CH4_inst = gd.k_inst_CH4.*(s_ch4-s_ch4_eq.*slp_inst)./1000.*sw_dens(s_S,s_T,s_P); % umol/m2/d
+gd.F_N2O_inst = gd.k_inst_N2O.*(s_n2o-s_n2o_eq.*slp_inst)./1000.*sw_dens(s_S,s_T,s_P); % umol/m2/d
+
+% calculate 30-day weighted gas transfer velocity corrected for ice cover
+gd.k_wt_30_CH4 = nan.*s_time; % initialize variable
+gd.k_wt_30_N2O = nan.*s_time; % initialize variable
+
+for kk = 1:length(Scmat_CH4(:,1))
+    gd.k_wt_30_CH4(kk) = kw_weighting(k.CH4(kk,:), int/24, wt_t, zMLmat(kk,:)); % m/d
+    gd.k_wt_30_N2O(kk) = kw_weighting(k.N2O(kk,:), int/24, wt_t, zMLmat(kk,:)); % m/d
+end
+
+clear kk
+
+slp_30 = mean(slpmat,2); % 30-day average slp
+
+%ice_inst = ice(:,end); % instantaneous ice fraction
+%ice_30 = mean(ice,2); % 30-day average ice fraction
+
+% calculate 30-day weighted sea-air flux of gases
+% Units are umol/m2/d = m/d * umol/kg * kg/m3
+gd.F_CH4_30 = gd.k_wt_30_CH4.*(s_ch4-s_ch4_eq.*slp_30)./1000.*sw_dens(s_S,s_T,s_P); % umol/m2/d
+gd.F_N2O_30 = gd.k_wt_30_N2O.*(s_n2o-s_n2o_eq.*slp_30)./1000.*sw_dens(s_S,s_T,s_P); % umol/m2/d
+disp('processed 30-day weighted gas fluxes')
+
+LIS_gas_flux = gd; %rename variable for saving
+
+
+%%
+A=find(b.datenum>=datenum(2023,7,15,0,0,0)&b.datenum<datenum(2023,8,15,0,0,0));
+k_JA = kgas(b.WSPD(A),Scmat_CH4(1),'W14').* spd; % m/d
+k_avg.CH4 = mean(k_JA);
+F_CH4_avg = k_avg.CH4.*(s_ch4-s_ch4_eq.*slp_30)./1000.*sw_dens(s_S,s_T,s_P); 
+
+%%
+
+%save LIS_N2O_flux.mat SBUS_N2O_flux; % save data
+
+% CH4 flux is averaging 83 umol/m2/d
+% N2O flux is averaging 1.41 umol/m2/d
+
+% We know that CO2 in WLIS ranges from 750-1500 uatm in summer
+% CO2 flux is around 13 mmol m-2 d-1
+% based on:
+% [F_CO2, dpCO2]=FCO2(1000,420,22,28,3)
+% CO2 flux is 13 000 umol m-2 d-1
+% CH4 flux is ~100 umol m-2 d-1 --> multiply by 10 to get CO2 equivalents
+% for GWP = 1000 umol m-2 d-1
+% at max, GWP = 3130
+
+
--- LIS/data/plot_LISICOS_winds.m
+++ LIS/data/plot_LISICOS_winds.m
@@ -1,37 +1,53 @@
+knots_to_meters_per_second = 0.514444;
 load exrx2023-2024_met.mat;
 exrxMet.windSpd_Kts(exrxMet.windSpd_Kts==0) = NaN;
+exrxMet.windSpd_MS = exrxMet.windSpd_Kts.* knots_to_meters_per_second;
 
+mbar_to_atm = 1./1013.25; % convert pressure data from mbar to atm for use in gas sol eqns
+
+exrxMet.baroPress_atm = exrxMet.baroPress_Avg .* mbar_to_atm;
+
+exrxMet_adj = exrxMet;
+
+
+save exrxMet_adj.mat exrxMet_adj;
+%%
 load wlis2023-2024_met.mat;
 A=find(wlisMet.windSpd_Kts>50);
 wlisMet.windSpd_Kts(A) = nan;
 wlisMet.windSpd_Kts(wlisMet.windSpd_Kts==0) = NaN;
+wlisMet.windSpd_MS = wlisMet.windSpd_Kts.* knots_to_meters_per_second;
 
 
 load clis2022-2024_met.mat;
 clisMet.windSpd_Kts(clisMet.windSpd_Kts==0) = NaN;
+clisMet.windSpd_MS = clisMet.windSpd_Kts.* knots_to_meters_per_second;
+
+clisMet_adj = clisMet;
+save clisMet_adj.mat clisMet_adj;
 
 %%
 
 % cruise dates
 % Aug 2-3 2023 - EXRX and CLIS
 % Oct 19-20 2023 - EXRX and CLIS
-% May 22-23, 2024 - 
+% May 22-23, 2024 - EXRX and CLIS only
 
 d1 = datetime(2023,9,10,0,0,0);
 d2 = datetime(2023,10,31,0,0,0);
-Ai = (exrxMet.TIMESTAMP>=d1) & (exrxMet.TIMESTAMP<=d2) & ~isnan(exrxMet.windSpd_Kts);
+Ai = (exrxMet.TIMESTAMP>=d1) & (exrxMet.TIMESTAMP<=d2) & ~isnan(exrxMet.windSpd_MS);
 A = find(Ai);
 if(isempty(A))
     disp('no values for exrx')
 end;
 
-Bi = (wlisMet.TIMESTAMP>=d1) & (wlisMet.TIMESTAMP<=d2) & ~isnan(wlisMet.windSpd_Kts);
+Bi = (wlisMet.TIMESTAMP>=d1) & (wlisMet.TIMESTAMP<=d2) & ~isnan(wlisMet.windSpd_MS);
 B = find(Bi);
 if(isempty(B))
     disp('no values for wlis')
 end;
 
-Ci = (clisMet.TIMESTAMP>=d1) & (clisMet.TIMESTAMP<=d2)  & ~isnan(clisMet.windSpd_Kts);
+Ci = (clisMet.TIMESTAMP>=d1) & (clisMet.TIMESTAMP<=d2)  & ~isnan(clisMet.windSpd_MS);
 C = find(Ci);
 if(isempty(C))
     disp('no values for clis')
@@ -40,37 +56,71 @@ end;
 % note
 figure(1)
 clf; hold on;
-plot(exrxMet.TIMESTAMP(A),exrxMet.windSpd_Kts(A));
-%plot(wlisMet.TIMESTAMP(B),wlisMet.windSpd_Kts(B));
-plot(clisMet.TIMESTAMP(C),clisMet.windSpd_Kts(C));
+plot(exrxMet.TIMESTAMP(A),exrxMet.windSpd_MS(A));
+%plot(wlisMet.TIMESTAMP(B),wlisMet.windSpd_MS(B));
+plot(clisMet.TIMESTAMP(C),clisMet.windSpd_MS(C));
 %legend('exrx','wlis','clis')
-ylim([0,40]);
+ylim([0,15]);
+
 
 %%
 dl = datetime(2022,1,1,0,0,0):15/60/24:datetime(2025,1,1,0,0,0);
+dl = datetime(2023,9,1,0,0,0):15/60/24:datetime(2023,11,1,0,0,0);
 
 % find the unique values in exrx
-[unique_times, ~, idx] = unique(exrxMet.TIMESTAMP);  % idx gives the group index for each key
-num_keys = numel(unique_times);
-avg_wspd_exrx = accumarray(idx(:), exrxMet.windSpd_Kts(:), [], @mean);
+[unique_times_exrx, ~, idx] = unique(exrxMet.TIMESTAMP);  % idx gives the group index for each key
+num_keys = numel(unique_times_exrx);
+avg_wspd_exrx = accumarray(idx(:), exrxMet.windSpd_MS(:), [], @mean);
 
-i_exrx = interp1(unique_times,avg_wspd_exrx,dl);
+i_exrx = interp1(unique_times_exrx,avg_wspd_exrx,dl); % interpolate to fill in the gaps
 
-[unique_times, ~, idx] = unique(wlisMet.TIMESTAMP);  % idx gives the group index for each key
-num_keys = numel(unique_times);
-avg_wspd_wlis = accumarray(idx(:), wlisMet.windSpd_Kts(:), [], @mean);
+wspd_exrx_dl = nan(size(dl));
 
-i_wlis = interp1(unique_times,avg_wspd_wlis,dl);
+for i = 1:length(dl)
+    A = find(unique_times_exrx == dl(i));
+    if ~isempty(A)
+        wspd_exrx_dl(i) = avg_wspd_exrx(A);
+    end;
+end;
 
-[unique_times, ~, idx] = unique(clisMet.TIMESTAMP);  % idx gives the group index for each key
-num_keys = numel(unique_times);
-avg_wspd_clis = accumarray(idx(:), clisMet.windSpd_Kts(:), [], @mean);
+% get a list of the nan values
+for i = 1:length(i_exrx)
+    A = find(unique_times_exrx == dl(i));
+    if ~isempty(A)
+        wspd_exrx_dl(i) = avg_wspd_exrx(A);
+    end;
+end;
 
-i_clis = interp1(unique_times,avg_wspd_clis,dl);
+%%
+figure(12)
+clf; hold on;
+plot(dl,wspd_exrx_dl);
+
+%%
+[unique_times_wlis, ~, idx] = unique(wlisMet.TIMESTAMP);  % idx gives the group index for each key
+num_keys = numel(unique_times_wlis);
+avg_wspd_wlis = accumarray(idx(:), wlisMet.windSpd_MS(:), [], @mean);
+
+i_wlis = interp1(unique_times_wlis,avg_wspd_wlis,dl); % interpolate to fill in the 
+
+[unique_times_clis, ~, idx] = unique(clisMet.TIMESTAMP);  % idx gives the group index for each key
+num_keys = numel(unique_times_clis);
+avg_wspd_clis = accumarray(idx(:), clisMet.windSpd_MS(:), [], @mean);
+
+i_clis = interp1(unique_times_clis,avg_wspd_clis,dl);
+
+wspd_clis_dl = nan(size(dl));
+
+for i = 1:length(dl)
+    A = find(unique_times_clis == dl(i));
+    if ~isempty(A)
+        wspd_clis_dl(i) = avg_wspd_clis(A);
+    end;
+end;
 
 %%
 x = i_exrx;  % data
-win = 21;    % size of the moving window (should be odd)
+win = 25;    % size of the moving window (must be odd)
 
 % Compute moving statistics
 mov_mean = movmean(x, win, 'omitnan');
@@ -89,11 +139,30 @@ x_smoothed(spike_idx) = mov_mean(spike_idx);
 
 figure(3)
 clf; hold on;
-plot(dl,i_exrx);
+plot(dl,i_exrx,'.');
 plot(dl,x_smoothed);
 
 %%
+% Linear fit (degree 1)
+pi = ~isnan(wspd_clis_dl) & ~isnan(wspd_exrx_dl);
+p = polyfit(wspd_clis_dl(pi), wspd_exrx_dl(pi), 1);
 
+% p(1) = slope, p(2) = intercept
+slope = p(1);
+intercept = p(2);
+
+figure(4)
+clf; hold on;
+scatter(wspd_clis_dl,wspd_exrx_dl);
+plot(wspd_clis_dl(pi),wspd_clis_dl(pi)*slope + intercept,'.');
+ylabel('exrx');
+xlabel('clis');
+
+figure(5)
+clf; hold on;
+plot(dl,wspd_exrx_dl);
+plot(dl,wspd_clis_dl);
+legend('exrx','clis');
 
 %%
 figure(2)
